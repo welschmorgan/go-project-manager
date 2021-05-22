7b497da187fd6f75c30f7fb9638fd2f12ccaf640
Morgan Welsch <welschmorgan@gmail.com>
2021-05-22 14:26:13 +0200

    Improve app config

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,10 +1,59 @@
 package config
 
+import "os"
+
 var (
 	// Used for flags.
-	CfgFile           string
-	WorkspacesRoot    string
-	WorkingDirectory  string
-	WorkspaceFilename string = ".grlm-workspace.yaml"
-	Verbose           bool
+	DefaultWorkspaceFilename string = ".grlm-workspace.yaml"
+	DefaultDevelopmentBranch string = "develop"
+	DefaultProductionBranch  string = "master"
+	DefaultVerbose           bool   = false
+
+	config *AppConfig
 )
+
+func init() {
+	var err error
+	if config, err = NewAppConfig(); err != nil {
+		panic(err.Error())
+	}
+}
+
+type BranchNamesConfig map[string]string
+
+type ReleaseConfig struct {
+	BrancheNames BranchNamesConfig
+}
+
+type AppConfig struct {
+	WorkspacesRoot    string
+	Release           ReleaseConfig
+	Verbose           bool
+	CfgFile           string
+	WorkingDirectory  string
+	WorkspaceFilename string
+}
+
+func NewAppConfig() (*AppConfig, error) {
+	if cwd, err := os.Getwd(); err != nil {
+		return nil, err
+	} else {
+		return &AppConfig{
+			WorkspacesRoot: "$HOME/development",
+			Release: ReleaseConfig{
+				BrancheNames: BranchNamesConfig{
+					"development": DefaultDevelopmentBranch,
+					"production":  DefaultProductionBranch,
+				},
+			},
+			Verbose:           DefaultVerbose,
+			CfgFile:           "",
+			WorkingDirectory:  cwd,
+			WorkspaceFilename: DefaultWorkspaceFilename,
+		}, nil
+	}
+}
+
+func Get() *AppConfig {
+	return config
+}
